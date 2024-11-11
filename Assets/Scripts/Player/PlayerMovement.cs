fe9a01ebae76583914e92682710b028eafa52034
guzanam <anamargs07@gmail.com>
2024-11-11 17:48:02 -0600

    sprotes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;  
-    public Transform groundCheck;
-    public float jumpForce = 5f;    
+    public float moveSpeed = 5f;     
+    public float jumpForce = 5f;     
+    public Transform groundCheck;    
     public LayerMask groundLayer;    
-    private Rigidbody2D rb;         
-    private bool isGrounded;        
+    private Rigidbody2D rb;          
+    private bool isGrounded;         
     private bool canJump = true;     
+    private float currentSpeed;      
 
     void Start()
     {
@@ -21,7 +20,6 @@ public class PlayerMovement : MonoBehaviour
 
         string currentScene = SceneManager.GetActiveScene().name;
 
-       
         if (currentScene == "BaseScene")
         {
             canJump = false;  
@@ -34,25 +32,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(transform.position, 0.1f, groundLayer);
-        float move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+        // Detecta si el personaje está en el suelo.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
-
+        // Procesa el salto
         if (Input.GetButtonDown("Jump") && isGrounded && canJump)
         {
             Jump();
         }
     }
 
-    //fixedupdate
+    void FixedUpdate()
+    {
+        // Movimiento horizontal con suavizado
+        float targetSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, 0.1f); // Ajusta el factor de suavizado (0.1f) según tus preferencias
+        rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+    }
 
     void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
-
-   
-    
 }
-
